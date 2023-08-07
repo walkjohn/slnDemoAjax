5d49a5fb93fca3d725798cea10bf357a622b6442
walkjohn <yeahmehaha@hotmail.com>
2023-08-07 17:12:31 +0800

    加入專案檔案。

---
+++ prjDemoAjax/Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjDemoAjax.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello Ajax!! First Ajax Program");
+        }
+    }
+}
